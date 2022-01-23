e575c62a41ca8d73b31cf11088d3edb7d5655fd9
Victor Oliveira <eu@victoroliveira.com.br>
2022-01-23 13:08:23 -0300

    feat(scraper): register_update_to_query util

--- scraper/utils/__init__.py
+++ scraper/utils/__init__.py
@@ -9,10 +9,26 @@ def add_datetime_to_object(obj):
     return obj
 
 
+def datetime_from_timestamp(timestamp):
+    if timestamp.find(':') == -1:
+        return datetime.utcnow()
+    hour, minute = map(int, timestamp.split(':'))
+    datetime_obj = datetime.utcnow().replace(hour=hour, minute=minute, second=0, microsecond=0)
+    return datetime_obj
+
+
 def register_update_to_object(obj):
     now = datetime.utcnow()
     obj['updated_at'] = now
     return obj
+
+
+# TODO: find better name
+def register_update_to_query(query):
+    if '$set' not in query.keys():
+        query['$set'] = {}
+    query['$set'] = register_update_to_object(query['$set'])
+    return query
 
 
 def id_as_object_id(_id):
