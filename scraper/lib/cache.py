d08cf97e21d8f5e4fea29e158e0128477b634edf
Victor Oliveira <eu@victoroliveira.com.br>
2022-01-23 17:15:45 -0300

    feat(scraper): cache tries db before raising error

--- scraper/lib/cache.py
+++ scraper/lib/cache.py
@@ -1,10 +1,16 @@
+from lib.db import Db
 from utils import add_datetime_to_object, register_update_to_object
 
 
 class Cache:
-    def __init__(self, strategy_fn=None):
+    def __init__(self, service, key_name='_id', strategy_fn=None, limit=100):
+        self.key_name = key_name
+        self.service = service
         self.strategy_fn = strategy_fn
         self.registry = {}
+        self.hits = 0
+        self.misses = 0
+        self.limit = limit
 
     def _enter(self, key, value, callback=None):
         self.registry[key] = value
@@ -14,12 +20,14 @@ class Cache:
         return value
 
     def check_entries(self):
-        if self.strategy_fn is None:
+        if self.strategy_fn is None or len(self.registry.keys()) < self.limit:
             return
-        keys = self.registry.keys()
-        for entry_key in keys:
-            if not self.strategy_fn(self.registry[entry_key]):
-                self.remove(entry_key)
+        removals = 0
+        for key in self.registry.keys():
+            if not self.strategy_fn(self.registry[key]):
+                self.remove(key)
+                removals += 1
+        # print('Number of removals: {}'.format(removals))
 
     def add(self, key, value, callback=None):
         obj = add_datetime_to_object({})
@@ -28,7 +36,13 @@ class Cache:
 
     def get(self, key):
         if key not in self.registry.keys():
-            raise KeyError('{} not found'.format(key))
+            obj = self.service.find({self.key_name: key})
+            if obj is None:
+                raise KeyError('{} does not exist'.format(key))
+            self.misses += 1
+            self.add(key, obj)
+        else:
+            self.hits += 1
         return self.registry[key]['value']
 
     def update(self, key, value, callback=None):
@@ -40,3 +54,10 @@ class Cache:
 
     def clear(self):
         self.registry = {}
+
+    @property
+    def stats(self):
+        return {
+            'hits': self.hits,
+            'misses': self.misses,
+        }
