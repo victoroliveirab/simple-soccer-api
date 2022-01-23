d15aab47d3a8482a7ebcb77dc6ca4a9d44852dd4
Victor Oliveira <eu@victoroliveira.com.br>
2022-01-23 13:10:29 -0300

    feat(scraper): .gitignore updated

---
+++ scraper/main.py
@@ -0,0 +1,11 @@
+from lib.db import Db
+from lib.parser import DefaultParser
+
+mongodb_conn_str = 'mongodb://admin:admin@127.0.0.1:27017/scraper-dev'
+
+if __name__ == '__main__':
+    Db.connect(mongodb_conn_str)
+    parser = DefaultParser()
+    with open('./test.txt') as text:
+        parser.parse(text)
+    print('Finished execution')
